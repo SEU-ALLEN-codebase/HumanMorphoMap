3628b4fd0c487ca63d8a9e6aa02af81d6ea869d1
crazylyf <crazylyf@163.com>
2025-03-10 16:27:42 +0800

    update the stem analyses

--- src/check_extreme_neurons.py
+++ src/check_extreme_neurons.py
@@ -13,6 +13,39 @@ import seaborn as sns
 from config import REG2LOBE
 
 def check_extreme_neurons(gf_file, meta_file, update_file, fn, display=False):
+
+    ############# helper functions #############
+    def histplot(top5_meta, meta, ftype='age'):
+        plt.figure(figsize=(6,6))
+        if ftype == 'age':
+            gname = f'{ftype}_group'
+            top5_meta[gname] = pd.cut(top5_meta[ftype], bins=range(18,85,10), right=False, 
+                                     labels=[f'{i}-{i+9}' for i in range(18, 75, 10)])
+            meta[gname] = pd.cut(meta[ftype], bins=range(18,85,10), right=False,
+                                     labels=[f'{i}-{i+9}' for i in range(18, 75, 10)])
+        else:
+            gname = ftype
+            
+        ftype_dist = top5_meta.groupby(gname, observed=True).size() / meta.groupby(gname, observed=True).size() * 100
+        ftype_dist.plot(kind='bar', color='lightsalmon')
+
+        plt.ylabel('Percentage of large-trees')
+        plt.xticks(rotation=0)
+        plt.xlabel(gname.capitalize())
+
+        ax = plt.gca()
+        ax.spines['left'].set_linewidth(2)
+        ax.spines['right'].set_linewidth(2)
+        ax.spines['top'].set_linewidth(2)
+        ax.spines['bottom'].set_linewidth(2)
+        ax.xaxis.set_tick_params(width=2)
+        ax.yaxis.set_tick_params(width=2)
+
+        plt.subplots_adjust(left=0.15, bottom=0.15)
+        plt.savefig(f'{ftype.split(" ")[-1]}.png', dpi=300); plt.close()
+
+    ######### End of helper functions ##########
+
     display_feats = [
         'N_stem',
         'Number of Branches',
@@ -29,7 +62,7 @@ def check_extreme_neurons(gf_file, meta_file, update_file, fn, display=False):
     dfu = pd.read_csv(update_file, index_col='cell_id_backUp', low_memory=False)
 
     # lobe-level distribution
-    top5_meta['Brain lobe'] = [REG2LOBE[r] for r in top5_meta['brain_region']]
+    meta['Brain lobe'] = [REG2LOBE[r] for r in meta['brain_region']]
 
     dfs = df[display_feats]
 
@@ -54,18 +87,13 @@ def check_extreme_neurons(gf_file, meta_file, update_file, fn, display=False):
     low5_meta_new_ids = dfu.loc[low5_meta.index]
 
     if display:
-        sns.set_theme(style='ticks', font_scale=1.8)
+        sns.set_theme(style='ticks', font_scale=1.7)
         # plot the distribution of top5 neurons with respect to different conditions
-        # 1. age
-        plt.figure(figsize=(6,6))
-        sns.histplot(data=top5_meta, x='age', bins=20); plt.savefig('age.png', dpi=300); plt.close()
-        # gender
-        plt.figure(figsize=(6,6))
-        sns.histplot(data=top5_meta, x='gender'); plt.savefig('gender.png', dpi=300); plt.close()
-        # across lobes
-        plt.figure(figsize=(6,6))
-        sns.histplot(data=top5_meta, x='Brain lobe'); plt.savefig('lobe.png', dpi=300); plt.close()
-        # across cell types
+        histplot(top5_meta, meta, ftype='age')
+        histplot(top5_meta, meta, ftype='gender')
+        histplot(top5_meta, meta, ftype='Brain lobe')
+
+        # TODO: across cell types
         
     
 
@@ -86,7 +114,7 @@ def plot_stem_distributions(datasets):
     # plot stripplots
     df_stems = pd.DataFrame(nstems)
     
-    sns.set_theme(style='ticks', font_scale=1.8)
+    sns.set_theme(style='ticks', font_scale=1.5)
     plt.figure(figsize=(6,6))
     sns.violinplot(df_stems, fill=False, cut=0)
 
@@ -122,16 +150,17 @@ if __name__ == '__main__':
     meta_file = '/data/kfchen/trace_ws/paper_trace_result/final_data_and_meta/meta.csv'
     update_file = '../meta/update_cellID_00001-50212.csv'
 
-    if 1:
+    if 0:
         fn = 'N_stem'
         check_extreme_neurons(gf_file, meta_file, update_file, fn, display=True)
 
-    if 0:
+    if 1:
         datasets = {
             'SEU-H8K': '/data/kfchen/trace_ws/paper_trace_result/final_data_and_meta_filter/l_measure_result.csv',
             'DeKock': '../resources/public_data/DeKock/intermediates/gf_one_point_soma_150um.csv',
             'Allen': '../resources/public_data/allen_human_neuromorpho/intermediates/gf_one_point_soma_150um.csv',
             'H01-Skel': './data/lmeausre_pyramidal_dendrites_annotation.csv',
+            #'Mouse-9K': '../../../parcellation/BrainParcellation/microenviron/plotters/me_proj/data/lm_features_dendrites100.csv'
         }
         plot_stem_distributions(datasets)
 
